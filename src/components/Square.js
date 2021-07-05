64b496a299b2367059a7bcc02b89a1b9553ad6e1
sab <sabiranazar591@gmail.com>
2021-07-05 21:12:01 +0530

    added board state and also react events

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,9 +1,9 @@
 /* eslint-disable react/button-has-type */
 import React from 'react';
 
-const Square = ({ value }) => {
+const Square = ({ value, onClick }) => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
